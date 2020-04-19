47ad2f6547f996cec5a61ed9f878730c7c26443a
Sean Dunaway <contactsean123@gmail.com>
2020-04-19 14:37:14 -0400

    Updates

---
+++ precache-manifest.a9528db6878b3e308b6f2a7f0da0d1c9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548413e3905a287c1800b894c1198e39",
+    "url": "/Samplr/index.html"
+  },
+  {
+    "revision": "9caf6ed66222e4f16875",
+    "url": "/Samplr/static/css/main.1bb8c340.chunk.css"
+  },
+  {
+    "revision": "65e1c126ef42d4c30db2",
+    "url": "/Samplr/static/js/2.fb48ab54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Samplr/static/js/2.fb48ab54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9caf6ed66222e4f16875",
+    "url": "/Samplr/static/js/main.f6bd1e6e.chunk.js"
+  },
+  {
+    "revision": "fcc1fe1e24b8f7eccd07",
+    "url": "/Samplr/static/js/runtime-main.90eebf37.js"
+  },
+  {
+    "revision": "fa75ccd9564b2098df83c195d442b89b",
+    "url": "/Samplr/static/media/button.fa75ccd9.png"
+  },
+  {
+    "revision": "2865761105a04228adcd360c046ea224",
+    "url": "/Samplr/static/media/buttonred.28657611.png"
+  },
+  {
+    "revision": "39f9581affa38e7f9322157d85e1ff57",
+    "url": "/Samplr/static/media/clap.39f9581a.wav"
+  },
+  {
+    "revision": "470b5edb9f8d1c23184513a3d5829967",
+    "url": "/Samplr/static/media/cowbell.470b5edb.wav"
+  },
+  {
+    "revision": "ff583b80d5adde611772a650a4d0f930",
+    "url": "/Samplr/static/media/crash.ff583b80.wav"
+  },
+  {
+    "revision": "3b1ce587938021ac040e1bd01c2c87b1",
+    "url": "/Samplr/static/media/hi-hat.3b1ce587.wav"
+  },
+  {
+    "revision": "987fa9f0c9b328ee9a9fe2801274b5fd",
+    "url": "/Samplr/static/media/kick.987fa9f0.wav"
+  },
+  {
+    "revision": "ef71b81965f0a8628b73c9c581778338",
+    "url": "/Samplr/static/media/metal.ef71b819.wav"
+  },
+  {
+    "revision": "1dcc600a6c41caa8ea7f43ee7435a10a",
+    "url": "/Samplr/static/media/openhihat.1dcc600a.wav"
+  },
+  {
+    "revision": "6d6d20935b3441bc151091c88b9e0ba8",
+    "url": "/Samplr/static/media/science.6d6d2093.wav"
+  },
+  {
+    "revision": "bc394356c54aa84cbb4bb0c8ce08447e",
+    "url": "/Samplr/static/media/snare.bc394356.wav"
+  },
+  {
+    "revision": "d97c0f96671c0383997d5865089e1081",
+    "url": "/Samplr/static/media/stab.d97c0f96.wav"
+  },
+  {
+    "revision": "446e78dd8d40065dfc499b4468610296",
+    "url": "/Samplr/static/media/tom.446e78dd.wav"
+  },
+  {
+    "revision": "390ac7fca78bcab9b15e356746c3118f",
+    "url": "/Samplr/static/media/tom2.390ac7fc.wav"
+  }
+]);
